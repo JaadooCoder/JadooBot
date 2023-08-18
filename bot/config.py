06d3433233d584f47aa79a8d58c15f3c20325292
JaadooCoder <142253484+JaadooCoder@users.noreply.github.com>
2023-08-18 09:55:38 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,5 @@
 class configClass:
-  api = ""
-  hash = ""
-  token = ""
-
+  api = "26483640"
+  hash = "bd5c67c15a48b6ab53bfcf199de8524a"
+  token = "6078597095:AAFJLnXGckkR09nLj5OlV_ybWzaFC1NfJJ8"
   
